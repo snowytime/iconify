ff9f730a641e7ec739d3625b580c8038e3af132e
Snaer <87044139+snowytime@users.noreply.github.com>
2022-11-28 17:05:24 -0700

    release

--- bin/release.ts
+++ bin/release.ts
@@ -1,5 +1,15 @@
-#!/usr/bin/env ts-node
+import { exec } from "node:child_process";
 
 /**
  * brings together all the other functions in building, verifying uniqueness, building readme, and releasing
  */
+
+export async function release() {
+	try {
+		exec('git add . && git commit -m "release" && git push origin master');
+	} catch (e) {
+		throw () => console.log(`❌ Error performing release.`);
+	}
+}
+
+release();
